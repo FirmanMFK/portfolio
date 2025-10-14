7e830c5383c83b48706eee511bac62cb7c620efc
unknown <firman.mfk@gmail.com>
2025-10-14 18:22:24 +0700

    change main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import MyTelkomselDetail from "./pages/MyTelkomselDetail";
@@ -16,16 +16,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/projects/mytelkomsel" element={<MyTelkomselDetail />} />
-          <Route path="/projects/digital-alumni-book" element={<DigitalAlumniDetail />} />
-          <Route path="/projects/bukku-e-commerce" element={<BukkuEcommerceDetail />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/projects/mytelkomsel" element={<MyTelkomselDetail />} />
+        <Route path="/projects/digital-alumni-book" element={<DigitalAlumniDetail />} />
+        <Route path="/projects/bukku-e-commerce" element={<BukkuEcommerceDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
